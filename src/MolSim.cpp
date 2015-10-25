83004eac3cda4e823291b0da8721ea17ab683f89
Jana <jana.kuzmanova@tum.de>
2015-10-25 22:17:25 +0100

    implemented calculation of force, position and velocity. added reading of end_time and delta_t from command line

--- src/MolSim.cpp
+++ src/MolSim.cpp
@@ -7,6 +7,7 @@
 #include <cstring>
 #include <cstdlib>
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
@@ -43,10 +44,15 @@ std::list<Particle> particles;
 int main(int argc, char* argsv[]) {
 
 	cout << "Hello from MolSim for PSE!" << endl;
-	if (argc != 2) {
+	if (argc != 4) {
 		cout << "Errounous programme call! " << endl;
 		cout << "./molsym filename" << endl;
 	}
+	else {
+		end_time = atof(argsv[2]);
+		delta_t = atof(argsv[3]);
+	}
+
 
 	FileReader fileReader;
 	fileReader.readFile(particles, argsv[1]);
@@ -94,7 +100,13 @@ void calculateF() {
 				Particle& p1 = *iterator;
 				Particle& p2 = *innerIterator;
 
-				// insert calculation of force here!
+				// calculate force
+
+				double denominator = pow(sqrt(pow(p1.getX()[0]-p2.getX()[0],2)+pow(p1.getX()[1]-p2.getX()[1],2)+pow(p1.getX()[2]-p2.getX()[2],2)),3);
+
+				p1.getF()[0] += p1.getM()*p2.getM()*(p2.getX()[0]-p1.getX()[0])/denominator;
+				p1.getF()[1] += p1.getM()*p2.getM()*(p2.getX()[1]-p1.getX()[1])/denominator;
+				p1.getF()[2] += p1.getM()*p2.getM()*(p2.getX()[2]-p1.getX()[2])/denominator;
 
 			}
 			++innerIterator;
@@ -111,6 +123,9 @@ void calculateX() {
 		Particle& p = *iterator;
 
 		// insert calculation of X here!
+		p.getX()[0] = p.getX()[0]+delta_t*p.getV()[0]+pow(delta_t,2)*p.getOldF()[0]/(2*p.getM());
+		p.getX()[1] = p.getX()[1]+delta_t*p.getV()[1]+pow(delta_t,2)*p.getOldF()[1]/(2*p.getM());
+		p.getX()[2] = p.getX()[2]+delta_t*p.getV()[2]+pow(delta_t,2)*p.getOldF()[2]/(2*p.getM());
 
 		++iterator;
 	}
@@ -124,6 +139,9 @@ void calculateV() {
 		Particle& p = *iterator;
 
 		// insert calculation of velocity here!
+		p.getV() = p.getV() + delta_t*(p.getOldF()[0]+p.getF()[0])/(2*p.getM());
+		p.getV() = p.getV() + delta_t*(p.getOldF()[1]+p.getF()[1])/(2*p.getM());
+		p.getV() = p.getV() + delta_t*(p.getOldF()[2]+p.getF()[2])/(2*p.getM());
 
 		++iterator;
 	}
